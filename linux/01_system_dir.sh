b7a60e756443e876aee8e0c3a54196f6c84fba0f
wuyuewen <suhupt_f@126.com>
2019-07-09 16:15:40 +0800

    linux学习

---
+++ linux/01_system_dir.sh
@@ -0,0 +1,40 @@
+#! /usr/bin
+:<<!
+系统启动必须：
+/boot 存放linux启动的内核文件
+/etc 存放配置文件
+/lib 存放基本的代码库
+/sys linux 2.6内核很大的一个变化
+指令集合
+/bin
+/sbin 系统管理员使用的程序和命令
+外部文件系统管理
+/dev: device的缩写，存放的是linux的外部设备
+/media U盘，光驱等
+/mnt 临时挂载的文件系统
+
+临时文件
+/run 存储系统启动以来的信息
+/lost+found 系统非法关机之后，存放一些文件
+/tmp
+
+账户
+/root
+/home
+/usr
+/usr/bin
+/usr/sbin
+/usr/src
+
+运行过程中需要
+/var
+/proc 管理内存空间
+
+扩展使用
+/opt
+/srv
+
+
+
+
+!
